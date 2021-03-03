f32ad8907d7bfca7d62318216c6ce8be67657fe5
Maheswari Prahalad <prahalad.maheswari@nagra.com>
2021-03-03 17:07:25 +0530

    Updated react version for using material ui components. Added basic components for Login Page

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import "./Login.css";
 import Header from '../../common/header/Header';
+import Card from '@material-ui/core/Card';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Button from '@material-ui/core/Button';
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            usernameRequired: "dispNone",
+            loginPassword: "",
+            loginPasswordRequired: "dispNone"
+        }
+    }
     render() {
         return (
             <div>
                 <Header />
-                Login Page
+                <div className="card-container">
+                <Card>
+                    <div className="card-content">
+                    <Typography variant="h5" component="h2">
+                        LOGIN
+                    </Typography>
+                    <br />
+                    <FormControl required>
+                        <InputLabel htmlFor="username"> Username </InputLabel>
+                        <Input id="username" type="text" username={this.state.username} />
+                    </FormControl>
+                    <br />
+                    <br />
+                    <FormControl required>
+                        <InputLabel htmlFor="Password"> Password </InputLabel>
+                        <Input id="password" type="password" password={this.state.loginPassword}/>
+                    </FormControl>
+                    <br/>
+                    <br/>
+                    <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
+                    </div>
+                </Card>
+                </div>
             </div>
         )
     }
