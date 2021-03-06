eb5675e5af49726fb44faffac7b5480ed3a474ff
Maheswari Prahalad <prahalad.maheswari@nagra.com>
2021-03-06 09:02:37 +0530

    Added search bar to the header menu

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,18 +1,72 @@
-import React, { Component } from 'react';
-import './Header.css';
+import React, { Component } from "react";
+import "./Header.css";
+import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import Input from "@material-ui/core/Input";
+
+const styles = (theme) => ({
+  search: {
+    position: "relative",
+    borderRadius: "4px",
+    backgroundColor: "#c0c0c0",
+    marginRight: theme.spacing(10),
+    marginLeft: 0,
+    width: "300px",
+    float: "right",
+    marginTop: "18px",
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+  },
+});
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                <header className="app-header">
-                    <span className="logo">
-                        Image Viewer
-                    </span>
-                </header>
+  constructor() {
+    super();
+  }
+
+  inputChangeHandler = (e) => {
+    console.log("INPUT CHANGE HANDLER !!!!");
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <header className="app-header">
+          <span className="logo">Image Viewer</span>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-        )
-    }
+            <Input
+              disableUnderline={true}
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+              onChange={this.inputChangeHandler}
+            />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
